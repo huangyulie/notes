cf14282868959bc4f87f6936d07ae314d8f39aeb
huangyulie <1279072972@qq.com>
2022-08-10 23:42:12 +0800

    123

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-let b ;
-
-function ajax() {
-    return new Promise((res)=>{
-        setTimeout(() => {
-            res('123123')
-        }, 1000);
-    })
-    // let xhr = new XMLHttpRequest();
-    // xhr.open("get", "https://netease-cloud-music-api-zeta-bice.vercel.app/top/playlist");
-    // xhr.onreadystatechange = () => {
-    //     if (xhr.readyState === 4) {
-    //         if (xhr.status >= 200 || xhr.status < 400) {
-    //             async(xhr.response);
-    //         }
-    //     }
-    // }
-    // xhr.send(null);
+function Super(name) {
+    this.name = name;
+    this.colors = ['red', 'black', 'white'];
 }
 
-async function async(){
-    b = await ajax();
-    console.log(b);
-    return '`12`1';
+Super.prototype.sayName = function(){
+    console.log(this.name);
 }
-let a;
-async().then((res)=>{});
 
-console.log(b);
+function Sub(name, age) {
+    Super.call(this, name);//采用盗用构造函数
+    this.age = age;
+    console.log(this);
+}
+
+Sub.prototype = new Super('Haha');//原型链继承
+
+Sub.prototype.sayAge = function(){
+    console.log(this.age);
+}
+
+let instance1 = new Sub('fuck',15);
+instance1.colors.push('green');
+console.log(instance1);
+
+let instance2 = new Sub('fuck',15);
+console.log(instance2);
