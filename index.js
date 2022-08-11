c2b4538f1375fc2a68c1bbbb70a2c1aa193a961c
huangyulie <1279072972@qq.com>
2022-08-11 21:52:51 +0800

    bind

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-function Super(name) {
-    this.name = name;
-    this.colors = ['red', 'black', 'white'];
+function fun(){
+    console.log(this,"this");
+}
+Function.prototype.MyBind = function(){
+    let arr = Array.from(arguments);
+    let t = arr.shift();
+    let that = this;
+    return function(){
+        let a = Symbol();
+        t[a] = that;
+        t[a]();
+        delete t[a];
+    }
 }
 
-Super.prototype.sayName = function(){
-    console.log(this.name);
-}
-
-function Sub(name, age) {
-    Super.call(this, name);//采用盗用构造函数
-    this.age = age;
-    console.log(this);
-}
-
-Sub.prototype = new Super('Haha');//原型链继承
-
-Sub.prototype.sayAge = function(){
-    console.log(this.age);
-}
-
-let instance1 = new Sub('fuck',15);
-instance1.colors.push('green');
-console.log(instance1);
-
-let instance2 = new Sub('fuck',15);
-console.log(instance2);
+let b = fun.MyBind({x:1});
+b();
---
+++ 前端面试知识/手写/手写bind.md
@@ -0,0 +1,20 @@
+```js
+function fun(){
+    console.log(this,"this");
+}
+Function.prototype.MyBind = function(){
+    let arr = Array.from(arguments);
+    let t = arr.shift();
+    let that = this;
+    return function(){
+        let a = Symbol();
+        t[a] = that;
+        t[a]();
+        delete t[a];
+    }
+}
+
+let b = fun.MyBind({x:1});
+b();
+
+```
